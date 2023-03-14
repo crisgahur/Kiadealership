5ee9be1fee0a54da876c3248958b060de7672a6e
Cristian Gallego <cris.gahur@hotmail.com>
2023-03-14 08:59:58 -0500

    Correcting code-review comments

--- sp_showSells.sql
+++
@@ -1,4 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_show_sells`()
-BEGIN
- SELECT * FROM SELL;
-END
--- sp_show_sell_employee1.sql
+++
@@ -1,2 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_show_sells_employee1`()
-SELECT * FROM SELL WHERE id_employee=1
